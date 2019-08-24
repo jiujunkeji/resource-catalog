e957013ab4d0143832a48db7f60e3778b5ebc175
bilinlin <1722655598@qq.com>
2019-08-24 10:52:58 +0800

    样式

--- renren-admin/src/main/resources/statics/js/modules/resource/resources.js
+++ renren-admin/src/main/resources/statics/js/modules/resource/resources.js
@@ -10,7 +10,34 @@ var vm = new Vue({
     methods:{
         // 元数据统计
         resourcemetedata:function () {
-
+            $.ajax({
+                type: "GET",
+                url: baseURL + "resource/resourcemetedata/stat",
+                success: function(r){
+                    if(r.code == 0){
+                        vm.gatntObj.userId = r.userList;
+                        layer.open({
+                            type: 1,
+                            title: '授权',
+                            content: $('#grant'), //这里content是一个普通的String
+                            skin: 'openClass',
+                            area: ['562px', '520px'],
+                            shadeClose: true,
+                            closeBtn:0,
+                            btn: ['确定','取消'],
+                            btn1:function (index) {
+                                vm.setGrant(id);
+                                layer.close(index);
+                            },
+                            btn2:function () {
+                                vm.reload();
+                            }
+                        })
+                    }else{
+                        layer.msg('<div class="okDiv"><img src="'+baseURL+'statics/img/fail.png"><br>操作失败</div>',{skin:'bg-class',area: ['400px', '270px']});
+                    }
+                }
+            });
         },
         // 访问量统计
         catalogsearch:function () {
