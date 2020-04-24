b46a87895b9075d7f4f1fc3fe401f7f22e91fcce
Administrator <2032900880@qq.com>
2020-04-24 16:48:06 +0800

    元数据集查询接口

--- renren-admin/src/main/java/io/renren/modules/xj/service/impl/XjMetaDataSetServiceImpl.java
+++ renren-admin/src/main/java/io/renren/modules/xj/service/impl/XjMetaDataSetServiceImpl.java
@@ -1,6 +1,9 @@
 package io.renren.modules.xj.service.impl;
 
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
@@ -25,5 +28,40 @@ public class XjMetaDataSetServiceImpl extends ServiceImpl<XjMetaDataSetDao, XjMe
 
         return new PageUtils(page);
     }
+
+    @Override
+    public PageUtils searchFindByMeteDataSetNumberOrName(Map<String, Object> params) {
+        String meteCategorySetId = (String) params.get("meteCategorySetId");
+        String meteSetNumber = (String) params.get("meteSetNumber");
+        String cnName = (String) params.get("cnName");
+        Page<XjMetaDataSetEntity> page =null;
+        if(StringUtils.isNotBlank(meteCategorySetId)){
+            if (StringUtils.isNotBlank(meteSetNumber) && StringUtils.isBlank(cnName)) {
+                page = this.selectPage(new Query<XjMetaDataSetEntity>(params).getPage(), new EntityWrapper<XjMetaDataSetEntity>().eq("mete_set_number", meteSetNumber).and().eq("mete_category_set_id",Long.valueOf(meteCategorySetId)));
+                return new PageUtils(page);
+            } else if (StringUtils.isNotBlank(cnName) && StringUtils.isBlank(cnName)) {
+                page = this.selectPage(new Query<XjMetaDataSetEntity>(params).getPage(), new EntityWrapper<XjMetaDataSetEntity>().eq("cn_name", cnName).and().eq("mete_category_set_id",Long.valueOf(meteCategorySetId)));
+                return new PageUtils(page);
+            }else if(StringUtils.isBlank(cnName) && StringUtils.isBlank(cnName)){
+                page = this.selectPage(new Query<XjMetaDataSetEntity>(params).getPage(), new EntityWrapper<XjMetaDataSetEntity>().eq("mete_category_set_id",Long.valueOf(meteCategorySetId)));
+                return new PageUtils(page);
+            }
+        }else{
+            if (StringUtils.isNotBlank(meteSetNumber) && StringUtils.isBlank(cnName)) {
+                page = this.selectPage(new Query<XjMetaDataSetEntity>(params).getPage(), new EntityWrapper<XjMetaDataSetEntity>().eq("mete_set_number", meteSetNumber));
+                return new PageUtils(page);
+            } else if (StringUtils.isNotBlank(cnName) && StringUtils.isBlank(cnName)) {
+                page = this.selectPage(new Query<XjMetaDataSetEntity>(params).getPage(), new EntityWrapper<XjMetaDataSetEntity>().eq("cn_name", cnName));
+                return new PageUtils(page);
+            }else if(StringUtils.isBlank(cnName) && StringUtils.isBlank(cnName)){
+               return queryPage(params);
+            }
+        }
+
+        return new PageUtils(page);
+    }
+
+
+
 
 }
